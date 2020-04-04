152429cec3eb1d526a08ae68047b5734308ad866
Coral Fussman <coralfussman@gmail.com>
2020-04-04 17:11:33 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,7 +1,7 @@
 def mario
   phrase = "It's-a me, Mario!\n"
   puts phrase
-  phrase
+  
 end
 
 
@@ -9,7 +9,7 @@ def toadstool
 
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
-  
+
 end
 
 
